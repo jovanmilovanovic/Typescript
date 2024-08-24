7afd9a5809c5a22e2cab0bf16a2d749ca72eba42
jovanmilovanovic <pressingreklame034@gmail.com>
2024-08-25 01:13:10 +0200

    Typescript primeri

--- prviTsProgram.js
+++ prviTsProgram.js
@@ -1,2 +1,27 @@
-var message = "Jovan";
-console.log(message);
+"use strict";
+class Song {
+    constructor(artist, title) {
+        this.artist = artist;
+        this.title = title;
+    }
+    play() {
+        console.log('Playing ' + this.title + " by " + this.artist);
+    }
+}
+class Playlist {
+    constructor(name) {
+        this.name = name;
+        this.songs = [];
+    }
+    addSoong(song) {
+        if (this.songs.length >= Playlist.maxSongCount) {
+            throw new Error('Playlist is full');
+        }
+        this.songs.push(song);
+    }
+}
+Playlist.maxSongCount = 30;
+const playlist = new Playlist("My playlist");
+const n = playlist.name;
+playlist.addSoong(new Song('Kylie Minogue', 'New York City'));
+const maxSongs = Playlist.maxSongCount;
--- prviTsProgram.ts
+++ prviTsProgram.ts
@@ -1,2 +1,25 @@
-let mesage:string = "Jovan";
-console.log(message);
+class Song {
+    constructor(private artist: string, private title:string) {}
+    play():void {
+        console.log('Playing ' + this.title + " by " + this.artist);
+        
+    }
+}
+
+class Playlist {
+        private songs: Song[] = []
+        public static readonly maxSongCount = 30;
+        constructor(public name: string) {}
+        addSoong(song: Song) {
+            if(this.songs.length >= Playlist.maxSongCount) {
+                throw new Error('Playlist is full')
+            }
+            this.songs.push(song)
+        }
+}
+
+
+const playlist =  new Playlist("My playlist");
+const n = playlist.name
+playlist.addSoong(new Song('Kylie Minogue', 'New York City'));
+const maxSongs = Playlist.maxSongCount
